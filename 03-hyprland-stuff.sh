2cf8c9edc9bfcc2bef25c1f22c673e8d42389130
gabtai <60150731+gabtai@users.noreply.github.com>
2025-03-02 14:02:49 +0100

    Update 03-hyprland-stuff.sh

--- 03-hyprland-stuff.sh
+++ 03-hyprland-stuff.sh
@@ -10,6 +10,9 @@ set -e
 sudo pacman -S --noconfirm --needed hypridle
 sudo pacman -S --noconfirm --needed hyprland
 sudo pacman -S --noconfirm --needed hyprlock
+sudo pacman -S --noconfirm --needed qt5-wayland
+sudo pacman -S --noconfirm --needed qt6-wayland
+sudo pacman -S --noconfirm --needed dunst
 #sudo pacman -S --noconfirm --needed hyprpaper
 #sudo pacman -S --noconfirm --needed hyprpicker
 sudo pacman -S --noconfirm --needed hyprpoliktagent
@@ -26,8 +29,10 @@ sudo pacman -S --noconfirm --needed wlogout
 #sudo pacman -S --noconfirm --needed mako
 #sudo pacman -S --noconfirm --needed lxappearance
 #sudo pacman -S --noconfirm --needed kvantum
-sudo pacman -S --noconfirm --needed wl-clipboard
+#sudo pacman -S --noconfirm --needed wl-clipboard
 #sudo pacman -S --noconfirm --needed archlinux-xdg-menu
 #sudo pacman -S --noconfirm --needed xdg-desktop-portal-kde
 #sudo pacman -S --noconfirm --needed xdg-desktop-portal-wlr
 #sudo pacman -S --noconfirm --needed xdg-desktop-portal-gtk
+
+paru -S tofi
