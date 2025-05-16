a81e6d3fd9bab64fafba4cbb168efae410853f99
gabtai <60150731+gabtai@users.noreply.github.com>
2025-05-16 12:18:16 +0200

    Update 03-hyprland-stuff.sh

--- 03-hyprland-stuff.sh
+++ 03-hyprland-stuff.sh
@@ -8,5 +8,5 @@ set -e
 
 paru -S --noconfirm --needed hyprland hyprpaper hyprlock hypridle hyprpicker xdg-desktop-portal-hyprland \
 libnotify kitty qt5-wayland qt6-wayland uwsm rofi-wayland polkit-gnome papirus-icon-theme flatpak swaync gvfs \
-bibata-cursor-theme-bin
+bibata-cursor-theme-bin waypaper
 
