607da09f8a9c6ff9f669072aa37ed6be7dc6b773
gabtai <60150731+gabtai@users.noreply.github.com>
2025-05-16 11:28:01 +0200

    Update 04-aur_program_install.sh

--- 04-aur_program_install.sh
+++ 04-aur_program_install.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 set -e
 
-paru -S --noconfirm --needed downgrade
-paru -S --noconfirm --needed google-chrome
-paru -S --noconfirm --needed shortwave
-paru -S --noconfirm --needed heroic-games-launcher-bin
-paru -S --noconfirm --needed capitaine-cursors
-paru -S --noconfirm --needed minecraft-launcher
-paru -S --noconfirm --needed jellyfin-media-player
-paru -S --noconfirm --needed video-downloader
-paru -S --noconfirm --needed spacecadetpinball-bin
-paru -S --noconfirm --needed protonup-qt
+paru -S --noconfirm --needed jellyfin-media-player matugen-bin
+
 
 ## MANGOHUD HASZNÁLATA STEAMBEN ## Indítási opciók-> mangohud %command% ##
 ## MANGOHUD HASZNÁLATA LUTRISBAN ## Beállítások -> System fül (alul advanced pipa be) Command prefix-hez -> mangohud
