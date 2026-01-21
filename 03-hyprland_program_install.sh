67020d9ac965fdaccb1d4e70473dde3f4c748250
gabtai <60150731+gabtai@users.noreply.github.com>
2026-01-21 11:30:57 +0100

    Add Heroic Games Launcher installation command

--- 03-hyprland_program_install.sh
+++ 03-hyprland_program_install.sh
@@ -35,6 +35,7 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 # Alkalmazások telepítése
 flatpak install -y flathub com.valvesoftware.Steam
 flatpak install -y flathub com.discordapp.Discord
+flatpak install -y flathub com.heroicgameslauncher.hgl
 
 # 4. AUR csomagok (már a yay-t használjuk)
 echo "AUR csomagok telepítése (Noctalia)..."
